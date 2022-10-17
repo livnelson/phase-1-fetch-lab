08ccaf6a23019d7baf83bf294956bc6f8de49e3b
Liv Nelson <livnelson@me.com>
2022-10-17 10:54:27 -0700

    commit tree d224294a9286b15d6be56208185a5d8af1557a6e

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+    // console.log(json)
 }
 
+fetchBooks();
+
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main');  // identify DOM element
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h2 = document.createElement('h2');  // create element
+    h2.innerText = book.name;
+    const pisbn = document.createElement('p');
+    pisbn.innerText = book.isbn;
+    const pcharacters = document.createElement('p');
+    pcharacters.innerText = book.characters;
+    // const ppages = document.createElement('p');
+    // ppages.innerText = book.pages;
+   
+    main.appendChild(h2, pisbn, pcharacters)
   });
 }
+
+// function pagesTotal(books) {
+//   for (let i=0; i,books.length; i++) {
+//     let pages = books[i].numberOfPages;
+//     let sum = books[i].numnberOfPages + books[i++].numberOfPages;
+//     return sum()
+//   }
+
+// const pages = book.map((book) => book.numberOfPages)
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
